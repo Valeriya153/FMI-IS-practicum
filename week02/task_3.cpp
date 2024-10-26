a7862192c827283fbc2267319fab219a538cfdcc
Valeriya153 <v.stoilova05@gmail.com>
2024-10-26 17:58:16 +0300

    Add files via upload

---
+++ week02/task_3.cpp
@@ -0,0 +1,23 @@
+﻿//По въведено естествено число n изведете n-тото число от редицата на Фибоначи.
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	
+	int n;
+	cout << "n = ";
+	cin >> n;
+
+	int fib1 = 0, fib2 = 1, temp;
+
+	for (int i = 2; i < n; i++) {
+
+		temp = fib1 + fib2;
+		fib1 = fib2;
+		fib2 = temp;
+	}
+
+	cout << fib2;
+	return 0;
+}
