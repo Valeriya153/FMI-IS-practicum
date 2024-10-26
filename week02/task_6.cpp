14e10b7f7a937ebb6abd271a47d8ef581a7933f5
Valeriya153 <v.stoilova05@gmail.com>
2024-10-26 18:12:49 +0300

    Add files via upload

---
+++ week02/task_6.cpp
@@ -0,0 +1,29 @@
+// зад. 6 Ќапишете програма, ко€то, по дадено число а и степенен показател n, намира а нa n-та степен.
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int a, n, sum = 1;
+
+	cout << "a = ";
+	cin >> a;
+
+	cout << "n = ";
+	cin >> n;
+
+	if (a == 0) {
+		cout << "1" << endl;
+	}
+	else {
+		for (int i = 1; i <= n; i++) {
+			sum *= a;
+		}
+	}
+
+	cout << sum << endl;
+
+	return 0;
+}
