d25aac19cf2e3452e96c1a2d6af10c2e60ac0260
Valeriya153 <v.stoilova05@gmail.com>
2024-10-26 18:18:03 +0300

    Add files via upload

---
+++ week02/task_8.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	for (char letter = 'A'; letter <= 'Z'; letter++) 
+	{
+
+		if (letter != 'A' && letter != 'E' && letter != 'I' && letter != 'O' && letter != 'U') 
+		{
+			cout << letter << ", ";
+		}
+	}
+
+	return 0;
+}
